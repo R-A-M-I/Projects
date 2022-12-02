501d3d217f312f2196b4755938675b7a042c9bce
R-A-M-I <96974371+R-A-M-I@users.noreply.github.com>
2022-12-03 02:58:19 +0300

    Add files via upload

---
+++ 1. Web Development Fundamentals/2. Dating Website/example.js
@@ -0,0 +1,18 @@
+let x = 100e10
+let y = "hello"
+let z = [1, 2, 3, 4, 5]
+let e = {
+    firstName:"John",
+    secondName:"Doe"
+};
+
+let w = null 
+let h = undefined 
+
+
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(w)
+console.log(h)
+console.log(e)
